5d76f57b8d6c9af6d3de76aa901d9b905826b275
Mike Shvartsman <ms44@princeton.edu>
2015-07-28 20:25:04 -0400

    Some informative messages for RScript

--- preprocess_tours.R
+++ preprocess_tours.R
@@ -1,6 +1,7 @@
 library(data.table)
 library(plyr)
 
+cat("Loading data...")
 # fread() does not play nicely with na.strings apparently
 tours <- data.table(read.csv('../safari-data/tours.csv', na.strings=c("NaN", "NA")))
 
@@ -15,7 +16,7 @@ tours[,trialWithinSector:=trial]
 tours[,trial:=1:.N,by="subject"]
 
 genProb <- matrix(c(0.1141, 0.2617, 0.2571, 0.4563, 0.1748, 0.0778, 0.1441, 0.2774, 0.0735, 0.0677, 0.1222, 0.0166, 0.3690, 0.2181, 0.2522, 0.0271, 0.2686, 0.3747, 0.2244, 0.2226), ncol=4, byrow=T)
-colSums(genProb) # sum to 1, sanity check
+# colSums(genProb) # sum to 1, sanity check
 
 nSectors <- ncol(genProb)
 nAnimals <- nrow(genProb)
@@ -34,6 +35,7 @@ getCounts <- function(d){
 }
 
 # get the posteriors (assuming dirichlet prior/post) for each timepoint
+cat("Computing counts by trial by subject by sector... ")
 counts <- daply(tours, .(subject), getCounts, .progress="text")
 counts <- adply(counts, 1:3, .progress="text") # get this back into a table (one row per sector per subject per trial)
 
@@ -47,12 +49,13 @@ counts$sector <- as.numeric(levels(counts$sector))[counts$sector]
 counts <- melt(counts, id.vars=c("subject","trial","sector"), variable.name="animal", value.name="countSoFar")
 
 # at a first stab, let's assume there's nothing interesting going on a sector other than the one we're in now. so let's just look up the counts into the data
-head(tours)
+cat("Merging animal running counts into dataset... ")
+
 tours <- merge.data.frame(tours, counts, by.x=c("subject", "trial", "sector","question_1"), by.y=c("subject","trial","sector","animal"), sort=F)
 colnames(tours)[15] <- "animal1Count"
 tours <- merge.data.frame(tours, counts, by.x=c("subject", "trial", "sector","question_2"), by.y=c("subject","trial","sector","animal"), sort=F)
 colnames(tours)[16] <- "animal2Count"
-head(tours)
+
 
 tours <- as.data.table(tours)
 
@@ -63,5 +66,5 @@ tours[,animal1Prior:=animal1Count/(trialsThisSector+1)]
 tours[,animal2Prior:=animal2Count/(trialsThisSector+1)]
 
 tours[,animalLogOdds:=log(animal1Prior/animal2Prior)]
-
+cat("Writing tours_preprocessed.csv")
 write.csv(tours, "tours_preprocessed.csv", row.names=F)
